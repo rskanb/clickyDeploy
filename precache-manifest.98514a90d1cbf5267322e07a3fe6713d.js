fe45df21ad46e98d06f646ab26601bf6b748c47b
rskanb <rs.kanbi@gmail.com>
2019-01-24 10:04:36 -0800

    Updates

---
+++ precache-manifest.98514a90d1cbf5267322e07a3fe6713d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d65b2234b7b542b0f95d",
+    "url": "/clickyDeploy/static/js/runtime~main.d65b2234.js"
+  },
+  {
+    "revision": "b6ae09f348345e8139b9",
+    "url": "/clickyDeploy/static/js/main.b6ae09f3.chunk.js"
+  },
+  {
+    "revision": "a18ecec28b82d28064bf",
+    "url": "/clickyDeploy/static/js/1.a18ecec2.chunk.js"
+  },
+  {
+    "revision": "b6ae09f348345e8139b9",
+    "url": "/clickyDeploy/static/css/main.08263e93.chunk.css"
+  },
+  {
+    "revision": "b8b98cd854d907235f021be732bc43c8",
+    "url": "/clickyDeploy/index.html"
+  }
+];
